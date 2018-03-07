b7488a1438f02f6b066f607f66b4e55709c7a0d3
SUP'Internet 05 <supinternet05@macbook-air-de-supinternet-7.local>
2018-03-07 14:05:46 +0100

    Initial Commit

---
+++ LeBonCoin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeBonCoin
+//
+//  Created by SUP'Internet 05 on 07/03/2018.
+//  Copyright © 2018 GM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
